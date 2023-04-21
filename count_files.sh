8feb23fc400366a7c51db46dc0959de8066904b3
Siddhant Chaudhary <urssidd@gmail.com>
2023-04-22 01:58:25 +0530

    Modifying code to count files in tsinghua data.

--- count_files.sh
+++ count_files.sh
@@ -1,6 +1,6 @@
-DIRECTORIES=( $(ls data/Images) )
+DIRECTORIES=( $(ls data/tsinghua/low-resolution) )
 
 for DIRECTORY in "${DIRECTORIES[@]}"
 do
-    ls -1 data/Images/$DIRECTORY | wc -l
+    ls -1 data/tsinghua/low-resolution/$DIRECTORY | wc -l
 done
